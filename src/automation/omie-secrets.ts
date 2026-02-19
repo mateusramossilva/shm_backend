24bc9bad55251f986d33071825891f92459c6810
mateusramossilva <mateusramos@4btaxtech.com.br>
2026-02-19 15:41:28 -0300

    Update omie-secrets.ts

--- src/automation/omie-secrets.ts
+++ src/automation/omie-secrets.ts
@@ -1,8 +1,6 @@
 export const OMIE_CONFIG = {
-    // Agora ele tenta ler do Railway/Computador, se não achar, usa o valor padrão
-    APP_KEY: process.env.APP_KEY || '2426459240205',
-    APP_SECRET: process.env.APP_SECRET || 'b354e6d4d43e7e6b779171b5691f9aa9',
-
-    // Configuração base da API
+    // O nome aqui deve ser igual ao do seu .env
+    APP_KEY: process.env.OMIE_APP_KEY || '2426459240205',
+    APP_SECRET: process.env.OMIE_APP_SECRET || 'b354e6d4d43e7e6b779171b5691f9aa9',
     BASE_URL: 'https://app.omie.com.br/api/v1'
 };
