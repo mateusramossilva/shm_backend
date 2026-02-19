1d2b8041a462db06a500db55c880a8b3cfa88819
mateusramossilva <mateusramos@4btaxtech.com.br>
2026-02-19 16:34:50 -0300

    package 7

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   engine: 'classic',
-  schema: './src/prisma/schema.prisma', // String pura aqui
+  schema: './src/prisma/schema.prisma',
   datasource: {
     url: process.env.DATABASE_URL,
   },
